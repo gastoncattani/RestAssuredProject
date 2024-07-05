148549fa00cf190fcd35e1296858420a25648c89
gastoncattani <gastoncattani@gmail.com>
2024-07-05 17:37:37 -0300

    POJO clases

--- src/main/java/oAuthTest.java
+++ src/main/java/oAuthTest.java
@@ -1,16 +1,26 @@
 import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pojo.Api;
+import pojo.GetCourse;
+import pojo.WebAutomation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 
 public class oAuthTest {
 
     @Test
-    public void oAuthTesting(){
+    public void oAuthTesting() {
+
+        String[] courseTitles = {"Selenium Webdriver Java", "Cypress", "Protractor"};
 
         // Authenticate to get access token
         String response = given()
-                .formParams("client_id","692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+                .formParams("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
                 .formParams("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
                 .formParams("grant_type", "client_credentials")
                 .formParams("scope", "trust")
@@ -20,12 +30,31 @@ public class oAuthTest {
 
         JsonPath js = new JsonPath(response);
 
-        String accessToken =  js.getString("access_token");
+        String accessToken = js.getString("access_token");
 
         // Get course detail with access token
-        String response2 = given().queryParam("access_token", accessToken)
-                .when().log().all().get("https://rahulshettyacademy.com/oauthapi/getCourseDetails").asString();
+        var getCourse = given().queryParam("access_token", accessToken)
+                .when().log().all().get("https://rahulshettyacademy.com/oauthapi/getCourseDetails").as(GetCourse.class);
 
-        System.out.println(response2);
+        System.out.println(getCourse.getLinkedIn());
+        System.out.println(getCourse.getInstructor());
+        System.out.println(getCourse.getCourses().getApi().get(1).getCourseTitle());
+
+        List<Api> apiCourses = getCourse.getCourses().getApi();
+        for(Api apiCourse : apiCourses){
+            if(apiCourse.getCourseTitle().equals("SoapUI Webservices testing")) {
+                System.out.println(apiCourse.getPrice());
+            }
+        }
+
+        // Get the course names of WebAutomation
+        ArrayList<String> a = new ArrayList<>();
+        List<WebAutomation> webAutomationCourses = getCourse.getCourses().getWebAutomation();
+        for(WebAutomation webAutomationCourse : webAutomationCourses){
+            a.add(webAutomationCourse.getCourseTitle());
+        }
+
+        List<String> expectedList = Arrays.asList(courseTitles);
+        Assert.assertTrue(a.equals(expectedList));
     }
 }
